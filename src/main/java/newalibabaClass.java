be9e11e5c47e10cc3cf21ac68e49ffbd7524fa6b
FawadjanAhmadi <117645848+FawadjanAhmadi@users.noreply.github.com>
2022-11-09 08:48:07 -0800

    Update newalibabaClass.java

--- src/main/java/newalibabaClass.java
+++ src/main/java/newalibabaClass.java
@@ -4,8 +4,7 @@ public class newalibabaClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 int a = 11;
-	}
-	public static void main(String[] args) {
+
 		// TODO Auto-generated method stub
 int b = 11;
 	}
